15592fde3a3a2b063cf3d809ebb7a7c1a806229c
Hemant <hemant.obdure@gmail.com>
2025-07-30 16:45:22 +0530

    send this flight to london

---
+++ multiply_or_add .py
@@ -0,0 +1,16 @@
+def multiply_or_add(a, b):
+    """Multiplies two numbers if their product is less than 1000, otherwise adds them."""
+    mul = a * b
+    if mul < 1000:
+        return mul
+    else:
+        return a + b
+
+# Taking input from user
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+# Calling the function and printing the result
+result = multiply_or_add(a, b)
+print("The result is:", result)
+
